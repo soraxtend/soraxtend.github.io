5949d1701d8471aa38022d81f01c6208a5c53434
Sarawoot <sarawoot.piyaarayanunt@gmail.com>
2019-11-13 01:26:55 +0700

    react

---
+++ precache-manifest.6feb30a5197bbf85c10a93e770a616bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c8cc1eed825d5fc56da561f6f22cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c96a54d8937f5ca283f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2e00e674f134ba5ee9d8",
+    "url": "/static/js/2.82a230ff.chunk.js"
+  },
+  {
+    "revision": "2c96a54d8937f5ca283f",
+    "url": "/static/js/main.33c56576.chunk.js"
+  },
+  {
+    "revision": "012f4df37fb11fc1af0e",
+    "url": "/static/js/runtime-main.154c106b.js"
+  }
+]);
